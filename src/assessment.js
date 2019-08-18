7860f3612fa4847cda5645cae673d07c1e91f854
Michal Koudelka <michal.koudelka@comjoo.com>
2019-08-18 22:08:46 +0200

    over and done

--- src/assessment.js
+++ src/assessment.js
@@ -1,5 +1,77 @@
+import dimensions from './dimensions';
+import { shuffle } from 'lodash';
 class Assessment {
+  constructor() {
+    this.questions = [];
+    this.results = {};
+    this.currentQuestion = 0;
+    this.generateNewTest();
+  }
 
+  generateNewTest() { 
+    const usedAnswers = {};
+    const chooseRandomAnswer = (dimensionIndex) => {
+      const { answers } = dimensions[dimensionIndex];
+      if (!usedAnswers[dimensionIndex]) usedAnswers[dimensionIndex] = [];
+      // The following line should never happen, it's just for implementation complexity
+      // if (answers.length === usedAnswers[dimensionIndex].length) throw Error('out of options');
+      const randomizedAnswers = shuffle(Array.from(answers.keys()));
+      for (const randomizedAnswerIndex in randomizedAnswers) {
+        if (usedAnswers[dimensionIndex].indexOf(randomizedAnswers[randomizedAnswerIndex]) === -1) {
+          return randomizedAnswers[randomizedAnswerIndex];
+        }
+      }
+      /* Alternative way how to solve the problem
+        const answerIndex = Math.floor(Math.random() * answers.length);
+        if (usedAnswers[dimensionIndex].indexOf(answerIndex) !== -1) {
+          return chooseRandomAnswer(dimensionIndex) //Hope to have more luck next time
+        }
+        this.usedAnswers[dimensionIndex].push(answerIndex);
+        return answerIndex;
+      */
+    }
+    for (const dimensionIndex1 in dimensions) {
+      for (const dimensionIndex2 in dimensions) {
+        if (dimensionIndex1 !== dimensionIndex2) {
+          this.questions.push(
+            [
+              {dimension: dimensionIndex1, answer: chooseRandomAnswer(dimensionIndex1)},
+              {dimension: dimensionIndex2, answer: chooseRandomAnswer(dimensionIndex2)},
+            ]
+          );
+        }
+      }
+    }
+    this.questions = shuffle(this.questions);
+    this.currentQuestion = 0;
+    for (const dimensionIndex in dimensions) {
+      this.results[dimensionIndex] = 0;
+    }
+  }
+
+  // Might be even easier witholut generator
+  * getQuestions() {
+   for (const question of this.questions) {
+     const questionObject = {
+      msg: "Pick the answer that describes you best:",
+      options: question.map((answer) => dimensions[answer.dimension].answers[answer.answer])
+     };
+     yield questionObject;
+     this.currentQuestion++;
+   }
+  }
+
+  submitAnswer(answerIndex) {
+    this.results[this.questions[this.currentQuestion][answerIndex].dimension] += 1;
+  }
+
+  get testResults() {
+    const testResults = {}
+    Object.keys(this.results).map((dimensionIndex) => {
+      testResults[dimensions[dimensionIndex].name] = this.results[dimensionIndex]
+    });
+    return testResults;
+  }
 }
 
 export default Assessment;
--- test/assessment.test.js
+++ test/assessment.test.js
@@ -1,25 +1,58 @@
-import Assessment from '../src/assessment'
+import Assessment from '../src/assessment';
 
+let assessment;
+let questionsGenerator;
 // Feel free to rewrite this test suite. This is provided as guidance.
 describe('The Assessment', () => {
+  beforeEach(() => {
+    assessment = new Assessment()
+  });
   it('should have 30 questions', () => {
-    expect('this test').toBe('failing');
+    expect(assessment.questions).toHaveLength(30);
   });
   it('should not show the same answer twice', () => {
-    expect('this test').toBe('failing');   
+    const answers = [];
+    for (const question  of assessment.questions) {
+      const serializedAnswer = question.map((answer) => `${answer.dimension}-${answer.answer}`);
+      expect(answers.indexOf(serializedAnswer)).toBe(-1);
+      answers.push(serializedAnswer);
+    }
   });
   it('should match each dimension to the other dimensions exactly 2 times', () => {
-    expect('this test').toBe('failing');   
+    const dimensions = [];
+    for (const question of assessment.questions) {
+      dimensions.push(
+        question.map((answer) => answer.dimension).sort().join('-')
+      );
+    }
+    dimensions.forEach((dimension) => {
+      expect(dimensions.filter((testedDimension) => testedDimension === dimension)).toHaveLength(2);
+    });
   });
   it('should provide ipsative questions (two possible answers)', () => {
-    expect('this test').toBe('failing');   
+    for (const question of assessment.questions) {
+      expect(question).toHaveLength(2);
+    }
   });
+
   describe('when completed', () => {
+    //Answer the questions
+    beforeEach(() => {
+      questionsGenerator = assessment.getQuestions();
+      for (const question of questionsGenerator) {
+        assessment.submitAnswer((Math.random() < 0.5) ? 0 : 1); //FIXME: Extend for n possible answers
+      }
+    })
     it('should provide the results as an object', () => {
-      expect('this test').toBe('failing');   
+      expect(typeof assessment.testResults).toBe('object');   
     });
     it('should represent the results based on 6 dimensions', () => {
-      expect('this test').toBe('failing');   
+      expect(Object.keys(assessment.testResults)).toHaveLength(6);   
+    });
+    it('number of scored points must be equal to 30', () => {
+      expect(
+        Object.values(assessment.testResults).reduce((accumulator, score) => (accumulator + score), 0)
+        ).toBe(30);
     });
   });
 });
